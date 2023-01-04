5bf289489c0ba68e923fe75bb736e7b6bb3c0774
Николай Балыков <balykovnikolay13@gmail.com>
2023-01-05 01:35:13 +0700

    Update code

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,9 +2,11 @@ from django.shortcuts import render, get_object_or_404
 
 from .models import Post, Group
 
+POST_COUNT = 10
+
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
+    posts = Post.objects.order_by('-pub_date')[:POST_COUNT]
     context = {
         'posts': posts,
     }
@@ -13,7 +15,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:POST_COUNT]
     context = {
         'group': group,
         'posts': posts,
